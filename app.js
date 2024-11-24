80c4a97f12c1a75d580c9e88016a57dd0c8b0616
Imjustrynnascriptxd <joagastaminza2@gmail.com>
2024-11-24 14:49:37 -0600

    Update app.js
    render thhing

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const apiBaseUrl = "https://files-vk0v.onrender.com/api/threads"; // Replace with your actual backend URL
+const apiBaseUrl = "https://render.com/docs/web-services#port-binding"; // Replace with your actual backend URL
 
 // Handle navigation
 function showPage(pageId) {
